dc306f96f1493327b7c02f4b5bdd3638fc9248aa
full_droper <full_droper@pm.me>
2022-07-14 04:34:33 +0300

    'auto-upd'

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const fastify = require('fastify')({logger: false})
 const {config} = require('./package.json')
+const {v4: uuidv4} = require('uuid')
+
 const db = new((require('mongodb')).MongoClient)(config.mongoDBURL, {
   authSource: "admin",
   useNewUrlParser: true
 })
 
-fastify.post('/reg', async (req, res) => {})
+fastify.post('/reg', async (req, res) => {
+  const {username, password} = req.body;
+  const connection = await db.connect();
+  const collection = await connection.db("blog").collection("users");
+  const isUserExist = await collection.findOne({username});
+
+  if (isUserExist) {
+    res.code(200).header("Error", "Username already exists").send();
+  } else {
+    const auth = {
+      token: uuidv4(),
+      refresh: uuidv4(),
+      lastUpdate: new Date().getTime()
+    }
+    await collection.insertOne({username, password, auth, id: auth.lastUpdate})
+    res.code(200).send({id: auth.lastUpdate, auth});
+  } connection.close()
+})
 
 fastify.listen(process.env.PORT || config.port || 80, (err, address) => {
   if (err) {
