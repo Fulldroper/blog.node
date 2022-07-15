a8f1df637d19440d385bd08fcf1ad3b154814cc0
full_droper <full_droper@pm.me>
2022-07-15 05:51:22 +0300

    'auto-upd'

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ const db = new((require('mongodb')).MongoClient)(config.mongoDBURL, {
   useNewUrlParser: true
 })
 
+const validateToken = async (token, collection) => {
+  const user = await collection.findOne({"auth.token": token});
+  if (new Date().getTime() - user.auth.lastUpdate > config.tokenLifeMS) {
+    return false;
+  } else {
+    return true
+  };
+};
+
 fastify.post('/reg', async (req, res) => {
   const {username, password} = req.body;
+  if (!username || !password) {
+    res.code(400).header("Error", "Empty parameters are entered").send();
+    return;
+  }
   const connection = await db.connect();
   const collection = await connection.db("blog").collection("users");
   const isUserExist = await collection.findOne({username});
 
   if (isUserExist) {
-    res.code(200).header("Error", "Username already exists").send();
+    res.code(406).header("Error", "Username already exists").send();
+    return;
   } else {
     const auth = {
       token: uuidv4(),
@@ -24,6 +38,33 @@ fastify.post('/reg', async (req, res) => {
     await collection.insertOne({username, password, auth, id: auth.lastUpdate})
     res.code(200).send({id: auth.lastUpdate, auth});
   } connection.close()
+})
+
+fastify.post('/update', async (req, res) => {
+  const {token, refresh} = req.body;
+  if (!token || !refresh) {
+    res.code(406).header("Error", "Empty parameters are entered").send();
+    return;
+  }
+  const connection = await db.connect();
+  const collection = await connection.db("blog").collection("users");
+  const user = await collection.findOne({"auth.token": token, "auth.refresh": refresh});
+  if (! user) {
+    res.code(404).header("Error", "Not Found").send();
+    return;
+  };
+
+  const auth = {
+    token: uuidv4(),
+    refresh: uuidv4(),
+    lastUpdate: new Date().getTime()
+  }
+
+  await collection.updateOne({
+    "id": user.id
+  }, {$set: {
+      auth
+    }})
 })
 
 fastify.listen(process.env.PORT || config.port || 80, (err, address) => {
